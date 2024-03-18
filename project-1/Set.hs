5facc01c6dbe2312ad90dc2e80829d7e7836bbcf
Dominik Wawszczak <wawszczakd@gmail.com>
2024-03-18 17:28:10 +0100

    Custom merge sort

--- project-1/Set.hs
+++ project-1/Set.hs
@@ -2,7 +2,6 @@ module Set(Set(..), empty, null, singleton, union, fromList
               , member, toList, toAscList, elems
               ) where
 import Prelude hiding(null)
-import Data.List (sort)
 
 data Set a = Empty
            | Singleton a
@@ -45,14 +44,36 @@ toAscList s =
     unique (h : t) =
       let
         helper :: Ord a => a -> [a] -> [a] -> [a]
-        helper prev []      acc = acc
-        helper prev (h : t) acc
-          | h == prev = helper h t acc
-          | otherwise = helper h t (h : acc)
+        helper prev []        acc = acc
+        helper prev (h' : t') acc
+          | h' == prev = helper h' t' acc
+          | otherwise  = helper h' t' (h' : acc)
       in
         reverse (helper h t [h])
+    
+    mergeSort :: Ord a => [a] -> [a]
+    mergeSort []  = []
+    mergeSort [x] = [x]
+    mergeSort l   =
+      let
+        split :: [a] -> ([a], [a]) -> Bool -> ([a], [a])
+        split []      (acc1, acc2) _     = (acc1, acc2)
+        split (h : t) (acc1, acc2) True  = split t (h : acc1, acc2) False
+        split (h : t) (acc1, acc2) False = split t (acc1, h : acc2) True
+        
+        merge :: Ord a => [a] -> [a] -> [a] -> [a]
+        merge []        []        acc = reverse acc
+        merge (h1 : t1) []        acc = merge t1 [] (h1 : acc)
+        merge []        (h2 : t2) acc = merge [] t2 (h2 : acc)
+        merge (h1 : t1) (h2 : t2) acc
+          | h1 <= h2  = merge t1 (h2 : t2) (h1 : acc)
+          | otherwise = merge (h1 : t1) t2 (h2 : acc)
+        
+        (one, two) = split l ([], []) True
+      in
+        merge (mergeSort one) (mergeSort two) []
   in
-    unique (sort (toList s))
+    unique (mergeSort (toList s))
 
 elems :: Set a -> [a]
 elems = toList
@@ -71,7 +92,6 @@ instance Semigroup (Set a) where
 
 instance Monoid (Set a) where
     mempty  = Empty
-    mappend = (<>)
 
 instance Show a => Show (Set a) where
     show Empty         = "_"
