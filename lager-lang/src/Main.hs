0d538b1827840c1b0df75d345d4ae1a97d1f523c
Dominik Wawszczak <wawszczakd@gmail.com>
2024-05-10 17:03:59 +0200

    Refactor

---
+++ lager-lang/src/Main.hs
@@ -0,0 +1,21 @@
+module Main where
+    import Interpreter
+    import System.Environment
+    import System.Exit
+    
+    main :: IO ()
+    main = do
+        args <- getArgs
+        case args of
+            ["--help"] -> do
+                putStrLn $ unlines
+                    [ "usage: Call with one of the following argument combinations:"
+                    , "  --help          Display this help message."
+                    , "  (no arguments)  Parse standard input."
+                    , "  (file)         Parse content of the file." ]
+                exitFailure
+            [f]        -> interpretFile f
+            []         -> interpretStdin
+            _          -> do
+                putStrLn "Invalid usage! Use --help."
+                exitFailure
